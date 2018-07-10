e6b9aaf03ad16b9405a195b8e76565f9a509e232
chengzichao <chengzichao@lansheng8.com>
2018-07-10 10:28:28 +0800

    使用promise重写

--- src/app.js
+++ src/app.js
@@ -1,72 +1,69 @@
-const request = require("request");
-const cheerio = require("cheerio");
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
-let proxyArr = [];
-
+const proxyArr = [];
 function fetchFastProxy() {
-  for (let i = 1; i <= 10; i++) {
-    const url = `https://www.kuaidaili.com/free/inha/${i}`;
-    request(
-      {
-        url: url,
-        method: "GET",
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
-        }
-      },
-      async (err, res, body) => {
-        if (err || !body) return;
-        const $ = cheerio.load(body);
-        const trList = $("tbody tr");
-        for (let i = 0; i < trList.length; i++) {
-          const tr = trList.eq(i);
-          const ip = tr
-            .children("td")
-            .eq(0)
-            .text();
-          const port = tr
-            .children("td")
-            .eq(1)
-            .text();
-          const protocol = tr
-            .children("td")
-            .eq(3)
-            .text()
-            .toLowerCase();
-          const proxy = new PROXY(ip, port, protocol);
-          const result = await check(proxy);
-          console.log(result);
-        }
+  for(let i = 1; i <= 1; i++) {
+    const url = `https://www.kuaidaili.com/free/inha/${ i }/`;
+    rp({
+      url: url,
+      method: "GET",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
       }
-    );
+    })
+    .then(async body => {
+      const $ = cheerio.load(body);
+      const trList = $('.table tbody tr');
+
+      for(let i = 0; i < trList.length; i++) {
+        const td = trList.eq(i).children('td');
+        const ip = td.eq(0).text();
+        const port = td.eq(1).text();
+        const protocol = td.eq(3).text().toLowerCase();
+        const proxy = new PROXY(ip, port, protocol);
+        const res = await check(proxy)
+        if(res) proxyArr.push(proxy);
+      }
+    })
+    .catch(err => {
+      console.log(`Sorry, failed to send request to ${ url }`);
+    })
   }
 }
 
-function PROXY(ip, port, protocol = "http") {
+function PROXY(ip, port, protocol) {
   this.ip = ip;
   this.port = port;
   this.protocol = protocol;
 }
 
-async function check(proxy) {
+
+function check(proxy) {
   const url = "http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js";
-  const proxyURL = `${proxy.protocol}://${proxy.ip}:${proxy.port}`;
-  await request(
-    {
-      url: url,
-      porxy: proxyURL,
-      method: "GET",
-      timeout: 20000
-    },
-    (err, res, body) => {
-      if (!err || res.statusCode !== 200 || !body) {
-        console.log(`${proxyURL} doesn't work.`);
-        return false;
-      }
-      console.log(`congratulations! ${proxyURL} works.`);
+  const proxyURL = `${ proxy.protocol }://${ proxy.ip }:${ proxy.port }`;
+  return rp({
+    url: url,
+    method: "GET",
+    resolveWithFullResponse: true,
+    proxy: proxyURL,
+    timeout: 20000
+  })
+  .then(res => {
+    if(res.statusCode === 200) {
+      console.log(`Congratulations, ${ proxyURL } works!`)
       return true;
-    }
-  );
+    };
+    console.log(`Sorry, ${ proxyURL } doesn't return 200`)
+    return false;
+  })
+  .catch(err => {
+    if(err) {
+      console.log(`Sorry, ${ proxyURL } doesn't work, ${ err.Error }`);
+      return false;
+    };
+  })
 }
-fetchFastProxy();
+
+
+fetchFastProxy()
